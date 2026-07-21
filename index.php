50ca026b9ba68f25cbe12daf475376d31c7f8f1d
Bazzcuy <nanamigemoy@gmail.com>
2026-07-21 09:44:37 +0700

    Simplify Hostinger root entrypoint

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 ﻿<?php
-declare(strict_types=1);
-
 $index = __DIR__ . '/public/index.html';
 if (is_file($index)) {
-    header('Content-Type: text/html; charset=utf-8');
     readfile($index);
     exit;
 }
-
 http_response_code(500);
 echo 'Index file not found.';
