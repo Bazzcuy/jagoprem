197c9e4d79a2f1fb1d92332040d1201000de48d9
Bazzcuy <nanamigemoy@gmail.com>
2026-07-21 09:34:33 +0700

    Fix Hostinger root routing

---
+++ index.php
@@ -0,0 +1,12 @@
+﻿<?php
+declare(strict_types=1);
+
+$index = __DIR__ . '/public/index.html';
+if (is_file($index)) {
+    header('Content-Type: text/html; charset=utf-8');
+    readfile($index);
+    exit;
+}
+
+http_response_code(500);
+echo 'Index file not found.';
